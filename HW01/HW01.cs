e3ec0d80e465e707687b7c1c0cd5a9cd34e9d0af
TheoBrin <timoxas677@outlook.com>
2023-05-13 19:51:46 +0600

    Добавьте файлы проекта.

---
+++ HW01/HW01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+class ImyaFamylya
+{
+    static void Main()
+    {
+        string name, surname, txt;
+        name = Interaction.InputBox("Введите ваше имя", "Ваше имя");
+        surname = Interaction.InputBox("Введите ваше фамилию", "Ваша фамилия");
+        txt = "Фамилия: " + surname + " Имя: " + name + ".";
+        MessageBox.Show(txt, "Карточка работника");
+    }
+}
